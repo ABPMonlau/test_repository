e8f3f3ba33ebc5e8d8a63aa300dbf597ff1c0b91
Pol Hernandez <polmx45@gmail.com>
2026-05-26 09:57:21 +0200

    NavBar prototipo creado

--- front-end-vinos/src/App.jsx
+++ front-end-vinos/src/App.jsx
@@ -1,8 +1,10 @@
+import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 
 function App() {
   return (
     <main className="min-h-screen bg-canal-bg">
+      <NavBar />
       <Hero />
     </main>
   )
