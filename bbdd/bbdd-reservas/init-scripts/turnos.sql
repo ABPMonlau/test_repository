23738cfc6aee9e2f7ea6f9f6376e11f500e2a8f8
Yangpeng Ni <yangpengni@gmail.com>
2026-05-27 11:09:23 +0200

    Delete bbdd/bbdd-reservas/init-scripts/turnos.sql

--- bbdd/bbdd-reservas/init-scripts/turnos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE turnos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    dia_semana ENUM('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') NOT NULL,
-    tipo_turno ENUM('maniana', 'comida', 'noche') NOT NULL,
-    hora_comienzo TIME NOT NULL,
-    hora_cierre TIME NOT NULL,
-    maxima_reserva INT NOT NULL
-)
