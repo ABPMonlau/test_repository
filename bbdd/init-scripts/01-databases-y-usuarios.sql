cd0ed725b82708ae7ea452779fccb2661ec8f45f
julenrejlop <julenrejlop@campus.monlau.com>
2026-05-27 09:06:30 +0200

    Nueva base de datos de usuario

--- bbdd/init-scripts/01-databases-y-usuarios.sql
+++ bbdd/init-scripts/01-databases-y-usuarios.sql
@@ -8,29 +8,38 @@
 -- --------------------------
 -- Base de datos: cataleg-vins
 -- --------------------------
-CREATE DATABASE IF NOT EXISTS `cataleg-vins`
-    DEFAULT CHARACTER SET utf8mb4
-    COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `cataleg-vins` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- --------------------------
 -- Base de datos: reservas_lacanal
 -- --------------------------
-CREATE DATABASE IF NOT EXISTS `reservas_lacanal`
-    DEFAULT CHARACTER SET utf8mb4
-    COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `reservas_lacanal` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+CREATE DATABASE IF NOT EXISTS ` users ` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- --------------------------
 -- Usuario: reservasadmin
 -- Permisos solo sobre reservas_lacanal
 -- --------------------------
-CREATE USER IF NOT EXISTS 'reservasadmin'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON `reservas_lacanal`.* TO 'reservasadmin'@'%';
+CREATE USER IF NOT EXISTS 'reservasadmin' @'%' IDENTIFIED BY '1234';
+
+GRANT ALL PRIVILEGES ON `reservas_lacanal`.* TO 'reservasadmin' @'%';
 
 -- --------------------------
 -- Usuario: vinosadmin
 -- Permisos solo sobre cataleg-vins
 -- --------------------------
-CREATE USER IF NOT EXISTS 'vinosadmin'@'%' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON `cataleg-vins`.* TO 'vinosadmin'@'%';
+CREATE USER IF NOT EXISTS 'vinosadmin' @'%' IDENTIFIED BY '1234';
+
+GRANT ALL PRIVILEGES ON `cataleg-vins`.* TO 'vinosadmin' @'%';
+
+-- --------------------------
+-- Usuario: useradmin
+-- Permisos solo sobre users
+-- --------------------------
+
+CREATE USER IF NOT EXISTS 'usersadmin' @'%' IDENTIFIED BY '1234';
+
+GRANT ALL PRIVILEGES ON ` users `.* TO 'usersadmin' @'%';
 
 FLUSH PRIVILEGES;
---
+++ bbdd/init-scripts/05-schema-users.sql
@@ -0,0 +1,17 @@
+USE users;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO
+    users (username, password_hash)
+VALUES (
+        'admin',
+        '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4'
+    )
+ON DUPLICATE KEY UPDATE
+    password_hash = VALUES(password_hash);
