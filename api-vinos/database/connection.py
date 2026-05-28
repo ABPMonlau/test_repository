be8f8cc3e10e3e11a14d0bfab36114144064018d
Pol Hernandez <polmx45@gmail.com>
2026-05-28 09:53:08 +0200

    Script datos Vinos

--- api-vinos/database/connection.py
+++ api-vinos/database/connection.py
@@ -7,7 +7,7 @@ def get_connection():
     Esta función se reutiliza en las rutas que necesitan consultar
     o modificar datos.
     """
-    connection = pymysql.connect(
+    connection_wine = pymysql.connect(
         host=os.environ.get("DB_HOST", "localhost"),
         port=int(os.environ.get("DB_PORT", 3306)),
         user=os.environ.get("DB_USER", "vinosadmin"),
@@ -15,4 +15,14 @@ def get_connection():
         database=os.environ.get("DB_NAME", "cataleg-vins"),
         cursorclass=pymysql.cursors.DictCursor,
     )
-    return connection
+
+    connection_menu = pymysql.connect(
+        host=os.environ.get("DB_HOST", "localhost"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        user=os.environ.get("DB_USER", ""),
+        password=os.environ.get("DB_PASSWORD", "1234"),
+        database=os.environ.get("DB_NAME", "cataleg-vins"),
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+    return [connection_wine, connection_menu]
