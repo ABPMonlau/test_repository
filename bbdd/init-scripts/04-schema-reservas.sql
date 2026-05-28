3e82e31fd1eb59a366a24aa6f06868af7a5c0997
Yangpeng Ni <Yangpengni@gmail.com>
2026-05-28 09:04:36 +0200

    Inserción de los turnos posibles

--- bbdd/init-scripts/04-schema-reservas.sql
+++ bbdd/init-scripts/04-schema-reservas.sql
@@ -69,6 +69,32 @@ CREATE TABLE `turnos` (
     `maxima_reserva` INT NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 
+INSERT INTO turnos (
+    dia_semana,
+    tipo_turno,
+    hora_comienzo,
+    hora_cierre,
+    maxima_reserva
+)
+VALUES
+-- Wednesday
+('Wednesday', 'comida', '13:00:00', '15:30:00', 30),
+
+-- Thursday
+('Thursday', 'comida', '13:00:00', '15:30:00', 30),
+
+-- Friday
+('Friday', 'comida', '13:00:00', '15:30:00', 30),
+('Friday', 'noche',  '20:00:00', '22:00:00', 30),
+
+-- Saturday
+('Saturday', 'maniana', '08:00:00', '10:30:00', 30),
+('Saturday', 'comida',  '13:00:00', '15:30:00', 30),
+('Saturday', 'noche',   '20:00:00', '22:00:00', 30),
+
+-- Sunday
+('Sunday', 'maniana', '08:00:00', '10:30:00', 30),
+('Sunday', 'comida',  '13:00:00', '15:30:00', 30);
 --
 -- Table structure for table `reservas`
 --
